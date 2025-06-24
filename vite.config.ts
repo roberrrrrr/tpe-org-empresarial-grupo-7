7bfa09833ec9c00b7ad685a6eb9c20c79fcdadf3
Martin Dieser <martindieser14@gmail.com>
2025-06-24 11:44:00 -0300

    modifque vite.config para deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['tpe-org-empresarial-grupo-7.onrender.com'],
+    port: 4173, 
+    host: true 
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
